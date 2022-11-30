2cd6a2c4c7509a867dfdc93b98e711d53702d475
KumarSuraj <surajk.ug19.ec@nitp.ac.in>
2022-12-01 00:08:54 +0530

    improved model added

--- car_drive.py
+++ car_drive.py
@@ -1,13 +1,10 @@
 import argparse
-import matplotlib.image as mpimg
 import numpy as np
 import cv2
 from keras.models import load_model     #load our saved model
 
 import base64   #decoding camera images
 
-import os       #reading and writing files       
-import shutil   #high level file operations
 
 import socketio     #real-time server
 import eventlet     #concurrent networking 
@@ -25,17 +22,11 @@ from io import BytesIO      #input output
 #initialize our server
 sio = socketio.Server()
 
-#our flask (web) app
+#flask (web) app
 app = Flask(__name__)
 
 
-#init our model and image array as empty
-
-model = None
-prev_image_array = None
-
-#set min/max speed for our autonomous car
-MAX_SPEED = 25
+MAX_SPEED = 18
 MIN_SPEED = 10
 
 #and a speed limit
@@ -48,6 +39,7 @@ def Preprocess(image):
     """
     #print("we are in preprocess")
     image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV) # Convert the image from RGB to YUV (This is what the NVIDIA model does)
+    #image = cv2.resize(image, (320, 80))
     image = image/255 # normalize pixels value between 0 & 1
     return image
 
@@ -61,23 +53,28 @@ def telemetry(sid, data):
     if data:
         # The current steering angle of the car
         steering_angle = float(data["steering_angle"])
+
         # The current throttle of the car, how hard to push peddle
-        
         throttle = float(data["throttle"])
+
         # The current speed of the car
-        
         speed = float(data["speed"])
+
         # The current image from the center camera of the car
-        
         image = Image.open(BytesIO(base64.b64decode(data["image"])))
+
+
         try:
             
             image = np.asarray(image)       # from PIL image to numpy array
             image = Preprocess(image)       # apply the preprocessing
             image = np.array([image])       # the model expects 4D array
             # predict the steering angle for the image
+            #print(image.shape)
             
             steering_angle = float(model.predict(image, batch_size=1))
+            #steering_angle = 0.2
+
             # lower the throttle as the speed increases
             # if the speed is above the current speed limit, we are on a downhill.
             # make sure we slow down first and then go back to the original max speed.
@@ -86,6 +83,7 @@ def telemetry(sid, data):
                 speed_limit = MIN_SPEED  # slow down
             else:
                 speed_limit = MAX_SPEED
+
             throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2
 
             print('{} {} {}'.format(steering_angle, throttle, speed))
